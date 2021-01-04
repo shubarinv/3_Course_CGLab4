044cd61e2c80ccc40fd91cbcec846ac3f628fe55
Vladimir Shubarin <vhundef@gmail.com>
2021-01-04 22:48:01 +0300

    Added required libs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+#include "application.hpp"
+void programQuit(int key, int action, Application *app) {
+  app->close();
+  LOG_S(INFO) << "Quiting...";
+}
+
+int main(int argc, char *argv[]) {
+  Application app;
+  app.init({640, 480}, argc, argv);
+  Application::setOpenGLFlags();
+  app.registerKeyCallback(GLFW_KEY_ESCAPE, programQuit);
+
+  while (!app.shouldClose) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	// 1rst attribute buffer : vertices
+	glCall(glfwSwapBuffers(app.getWindow()->getGLFWWindow()));
+	glfwPollEvents();
+  }
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
 }
